e38bf747b07cc5617abbfb8472d55cf28eadb2bd
Ilya Saenko <mr.journalist@inbox.ru>
2023-08-22 01:29:07 +0300

    auth

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -6,7 +6,7 @@ export function register(password, email) {
 	return fetch(`${BASE_URL}/signup`, {
 		method: 'POST',
 		headers: {
-			'Content-Type': 'application/json'
+			"Content-Type": 'application/json'
 		},
 		body: JSON.stringify({ password, email })
 	})
@@ -17,19 +17,19 @@ export function authorize(password, email) {
 	return fetch(`${BASE_URL}/signin`, {
 		method: 'POST',
 		headers: {
-			'Content-Type': 'application/json'
+			"Content-Type": 'application/json'
 		},
 		body: JSON.stringify({ password, email })
 	})
 		.then(checkResponse)
 };
 
-export function checkToken(token) {
+export function checkToken() {
 	return fetch(`${BASE_URL}/users/me`, {
 		method: 'GET',
 		headers: {
 			"Content-Type": "application/json",
-			"Authorization": `Bearer ${token}`
+			Authorization: `Bearer ${localStorage.getItem('jwt')}`
 		}
 	})
 		.then(checkResponse)
