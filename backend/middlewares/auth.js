d0864302c54ec75e0beb223e1c6f9a97bcdc3fd7
Ilya Saenko <mr.journalist@inbox.ru>
2023-08-22 00:27:30 +0300

    fix auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,25 +4,44 @@ const UnauthorizedError = require('../errors/unauthorizedError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  const bearer = 'Bearer ';
 
-  if (!authorization || !authorization.startsWith(bearer)) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     return next(new UnauthorizedError('Неправильные почта или пароль'));
   }
-
-  const token = authorization.replace(bearer, '');
+  const token = authorization.replace('Bearer ', '');
   let payload;
-
   try {
     payload = jwt.verify(token, SUPER_SECRET_KEY);
+    // payload = jwt.verify(token, 'very-secret-key');
+    // console.log('success payload:', payload);
   } catch (err) {
     return next(new UnauthorizedError('Неправильные почта или пароль'));
   }
-
   req.user = payload;
-
   return next();
 };
+
+// module.exports = (req, res, next) => {
+//   const { authorization } = req.headers;
+//   const bearer = 'Bearer ';
+
+//   if (!authorization || !authorization.startsWith(bearer)) {
+//     return next(new UnauthorizedError('Неправильные почта или пароль'));
+//   }
+
+//   const token = authorization.replace(bearer, '');
+//   let payload;
+
+//   try {
+//     payload = jwt.verify(token, SUPER_SECRET_KEY);
+//   } catch (err) {
+//     return next(new UnauthorizedError('Неправильные почта или пароль'));
+//   }
+
+//   req.user = payload;
+
+//   return next();
+// };
 
 // module.exports = (req, res, next) => {
 //   if (!req.cookies.jwt) {
